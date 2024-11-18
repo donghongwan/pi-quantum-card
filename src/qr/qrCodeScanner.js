4be9658edd4cc86ed3ae9a13961fc67024fb7598
KOSASIH <kosasihg88@gmail.com>
2024-11-18 20:32:00 +0700

    Create qrCodeScanner.js

---
+++ src/qr/qrCodeScanner.js
@@ -0,0 +1,32 @@
+// src/qr/qrCodeScanner.js
+
+const jsQR = require('jsqr');
+
+class QRCodeScanner {
+    static scanQRCode(imageData, width, height) {
+        const code = jsQR(imageData, width, height);
+        if (code) {
+            console.log('QR Code scanned successfully:', code.data);
+            return code.data;
+        } else {
+            console.log('No QR Code found.');
+            return null;
+        }
+    }
+}
+
+module.exports = QRCodeScanner;
+
+// Example usage:
+// const fs = require('fs');
+// const { createCanvas, loadImage } = require('canvas');
+
+// (async () => {
+//     const image = await loadImage('path/to/qr-code-image.png');
+//     const canvas = createCanvas(image.width, image.height);
+//     const ctx = canvas.getContext('2d');
+//     ctx.drawImage(image, 0, 0);
+//     const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+//     const result = QRCodeScanner.scanQRCode(imageData, canvas.width, canvas.height);
+//     console.log(result);
+// })();
